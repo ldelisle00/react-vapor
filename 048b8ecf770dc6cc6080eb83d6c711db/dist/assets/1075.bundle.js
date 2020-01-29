ca97e936c9dac6e41cc9909152cf3f52e1edf67c
Travis CI User <travis@example.org>
2020-01-29 20:30:22 +0000

    ci(demo): demo for branch: feature-generic-validation

--- 048b8ecf770dc6cc6080eb83d6c711db/dist/assets/1075.bundle.js
+++ 048b8ecf770dc6cc6080eb83d6c711db/dist/assets/1075.bundle.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1075],{2223:function(n,r,i){"use strict";i.r(r),r.default=".inputBlock {\r\n    margin-top: 0;\r\n}\r\n\r\n.input {\r\n    flex: auto;\r\n    margin-right: 2em;\r\n    margin-bottom: 1em;\r\n}\r\n\r\n.input:last-child {\r\n    margin-right: 0;\r\n    padding-right: 2em;\r\n}\r\n"}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1075],{2223:function(s,e,n){"use strict";n.r(e),e.default="@import '~coveo-styleguide/scss/common/palette.scss';\r\n@import '~coveo-styleguide/scss/variables.scss';\r\n\r\n.singleSelectFixedWidth {\r\n    width: $dropdown-button-min-width;\r\n}\r\n"}}]);
 //# sourceMappingURL=1075.bundle.js.map
