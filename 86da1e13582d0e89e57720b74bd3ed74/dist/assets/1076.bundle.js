b3edc610136fed0d540c35f344d0774a58b8fc56
Travis CI User <travis@example.org>
2020-02-10 20:59:23 +0000

    ci(demo): demo for branch: feature-button-validation

---
+++ 86da1e13582d0e89e57720b74bd3ed74/dist/assets/1076.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1076],{2230:function(s,e,n){"use strict";n.r(e),e.default="@import '~coveo-styleguide/scss/common/palette.scss';\r\n@import '~coveo-styleguide/scss/variables.scss';\r\n\r\n.singleSelectFixedWidth {\r\n    width: $dropdown-button-min-width;\r\n}\r\n"}}]);
+//# sourceMappingURL=1076.bundle.js.map
---
+++ 86da1e13582d0e89e57720b74bd3ed74/dist/assets/52.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[52],{1103:function(o,n,p){"use strict";p.r(n),n.default="export * from './LogoCard';\n"}}]);
+//# sourceMappingURL=52.bundle.js.map
