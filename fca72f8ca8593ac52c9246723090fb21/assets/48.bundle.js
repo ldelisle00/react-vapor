c07f0b98467a9a76fa578fec8a270b0acd2deddf
Travis CI User <travis@example.org>
2020-02-10 19:46:30 +0000

    ci(demo): demo for branch: clean-typography-section

---
+++ fca72f8ca8593ac52c9246723090fb21/assets/48.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[48],{1098:function(e,n,o){"use strict";o.r(n),n.default="export * from './LabeledValue';\n"}}]);
+//# sourceMappingURL=48.bundle.js.map
