097e640a5d71d7419d90e5488832ff6e3a5e71f7
Travis CI User <travis@example.org>
2020-02-12 16:38:45 +0000

    ci(demo): demo for branch: clean-icons-section

---
+++ 18081c4ef9c6d1645884a734510ef309/dist/assets/27.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{1077:function(n,o,t){"use strict";t.r(o),o.default="export * from './Content';\n"}}]);
+//# sourceMappingURL=27.bundle.js.map
