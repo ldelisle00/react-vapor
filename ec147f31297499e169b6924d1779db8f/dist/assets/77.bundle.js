be0dbf9107f47e79a6334c57bf18a462681f2b0c
Travis CI User <travis@example.org>
2020-02-20 16:33:17 +0000

    ci(demo): demo for branch: add-blacklist-card-style

---
+++ ec147f31297499e169b6924d1779db8f/dist/assets/77.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[77],{1129:function(n,o,t){"use strict";t.r(o),o.default="export * from './StatusCard';\n"}}]);
+//# sourceMappingURL=77.bundle.js.map
---
+++ ec147f31297499e169b6924d1779db8f/dist/assets/96.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[96],{1148:function(n,i,o){"use strict";o.r(i),i.default="export * from './withEditing';\n"}}]);
+//# sourceMappingURL=96.bundle.js.map
