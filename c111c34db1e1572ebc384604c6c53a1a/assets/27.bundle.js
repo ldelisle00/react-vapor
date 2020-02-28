41090699c0329ed06e5d13253a413b6bbb55a867
Travis CI User <travis@example.org>
2020-02-28 22:33:58 +0000

    ci(demo): demo for branch: fix-ADUI-5227-autoFocus-not-autofocus

---
+++ c111c34db1e1572ebc384604c6c53a1a/assets/27.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{211:function(n,r,t){"use strict";t.r(r),r.default="export const squareContainer = {\r\n    width: '200px',\r\n    height: '200px',\r\n};\r\n\r\nexport const rectangleContainer = {\r\n    width: '200px',\r\n    height: '100px',\r\n};\r\n"}}]);
+//# sourceMappingURL=27.bundle.js.map
