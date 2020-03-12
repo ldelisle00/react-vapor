c0b806e2d34561adbd48e401eed428c4d849234c
Travis CI User <travis@example.org>
2020-03-12 14:37:35 +0000

    ci(demo): demo for branch: fix-es5-react-vapor-lib

--- cd103fe5d50f0c5eb06b657cc7e6dcfb/dist/assets/70.bundle.js
+++ cd103fe5d50f0c5eb06b657cc7e6dcfb/dist/assets/70.bundle.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[70],{187:function(n,o,e){"use strict";e.r(o),o.default="## Guidelines\n\nDo not display more than one Popover at a time to keep interactions simple and avoid confusion.\n\n---\n\n## Related Components\n\n[Tooltip](#/components/Tooltip)\n"}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[70],{185:function(n,o,e){"use strict";e.r(o),o.default="## Guidelines\n\nDo not display more than one Popover at a time to keep interactions simple and avoid confusion.\n\n---\n\n## Related Components\n\n[Tooltip](#/components/Tooltip)\n"}}]);
 //# sourceMappingURL=70.bundle.js.map
--- cd103fe5d50f0c5eb06b657cc7e6dcfb/dist/assets/74.bundle.js
+++ cd103fe5d50f0c5eb06b657cc7e6dcfb/dist/assets/74.bundle.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[74],{189:function(n,e,o){"use strict";o.r(e),e.default="## Guidelines\n\nUse refresh component to trigger a callback after a number of X seconds.\n\n---\n"}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[74],{187:function(n,e,o){"use strict";o.r(e),e.default="## Guidelines\n\nUse refresh component to trigger a callback after a number of X seconds.\n\n---\n"}}]);
 //# sourceMappingURL=74.bundle.js.map
